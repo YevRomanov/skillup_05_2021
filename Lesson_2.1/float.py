801e6211ecf8df682f4b7ea7d21ef0883de73501
YevRomanov <yevromanov@gmail.com>
2021-06-08 12:30:14 +0200

    commit

---
+++ Lesson_2.1/float.py
@@ -0,0 +1,14 @@
+a = float(1)
+b = float("1")
+c = float("-1.0666")
+d = float("-infinity")
+
+print(a, b, c, d)
+print(type(c))
+print(type(d))
+
+i = "5.05"
+print("type a", type(i))
+
+z = "7"
+print("type z", type(z))
