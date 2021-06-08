3e8e52d1bb0493ddd9def448706e8a41cc799152
YevRomanov <yevromanov@gmail.com>
2021-06-08 12:03:34 +0200

    commit

---
+++ Lesson_2.1/bin.py
@@ -0,0 +1,27 @@
+# ONLY INTEGERS!!!
+
+bin(1)
+print(bin(1))
+bin(2)
+print(bin(2))
+bin(3)
+print(bin(3))
+bin(4)
+print(bin(4))
+
+bin(-1)
+print(bin(-1))
+bin(-2)
+print(bin(-2))
+bin(-3)
+print(bin(-3))
+bin(-4)
+print(bin(-4))
+
+bin(0x4)
+print(bin(0x4))
+
+bin(1)
+print(bin(1))
+type(bin(1))
+print(type(bin(1)))
