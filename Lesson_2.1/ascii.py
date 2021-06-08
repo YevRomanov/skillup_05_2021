f23ba3f132fad7b518c90acf7cb7a84a4a8d37cf
YevRomanov <yevromanov@gmail.com>
2021-06-08 11:52:34 +0200

    commit

---
+++ Lesson_2.1/ascii.py
@@ -0,0 +1,17 @@
+# 32 - 127
+# list(range(32, 128))
+
+range(32, 128)
+list(range(32, 128))
+print(list(range(32, 128)))
+
+for i in range(32, 128):
+    print(chr(i), end = " ")
+    if i % 10 == 0:
+        print()
+
+
+
+
+
+
