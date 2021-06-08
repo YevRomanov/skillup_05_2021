242e64ea87d9b4e09540f992b46a5fe4a14cc62d
YevRomanov <yevromanov@gmail.com>
2021-06-08 13:04:26 +0200

    commit

---
+++ Lesson_2.1/print.py
@@ -0,0 +1,9 @@
+a = "Hello, how can i help you?"
+b = "Hello, do you have any socks there?"
+print(a)
+print(b)
+print(a, b, end = " ", sep='->')
+
+c = "Of course!:) What size and colour do you prefer?"
+d = "I need only one pair in black:)"
+print(c, d, end = " ", sep='->')
