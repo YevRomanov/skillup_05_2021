aff6aa1ec9ab876bc14e49f6af585f1760efa058
YevRomanov <yevromanov@gmail.com>
2021-06-11 14:36:50 +0200

    commit

---
+++ Lesson_2.2/2.py
@@ -0,0 +1,7 @@
+a = input("Введите имя пользователя: ")
+b = input("Введите фамилию пользователя: ")
+print("Поздравляем с успешной авторизацией, уважаемый", a, b, ":)")
+
+user_names = (input("Enter your name: ") + " " + input("Enter your surname: "))
+print("Hey, dear", user_names, ":)")
+
