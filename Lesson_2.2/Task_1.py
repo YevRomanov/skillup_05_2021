e1006ee5558627339ade1fc1e39b00a8f82a0007
YevRomanov <yevromanov@gmail.com>
2021-06-13 20:28:49 +0200

    commit

--- Lesson_2.2/Task_1.py
+++ Lesson_2.2/Task_1.py
@@ -1,13 +1,13 @@
 
-a = float(input("Enter first numer: "))
-b = float(input("Enter second numer: "))
-c = float(input("Enter third numer: "))
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+c = float(input("Enter third number: "))
 s = (a+b+c)
 print("Total amount:", s)
 
-a = float(input("Enter first numer: "))
-b = float(input("Enter second numer: "))
-c = float(input("Enter third numer: "))
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+c = float(input("Enter third number: "))
 p = (a*b*c)
 print("Total amount:", p)
 
