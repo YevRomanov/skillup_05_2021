d5944ddd6ed0adb5354c2b10d1b6132ba431c1bb
YevRomanov <yevromanov@gmail.com>
2021-07-05 13:01:44 +0200

    commit

---
+++ Functions/2.py
@@ -0,0 +1,32 @@
+# def header(func):
+
+#     def inner(*args, **kwargs):
+#         print("<h1>")
+#         func(*args, **kwargs)
+#         print("</h1>")
+#     return inner
+
+# def table(func):
+
+#     def inner(*args, **kwargs):
+#         print("<table>")
+#         func(*args, **kwargs)
+#         print("</table>")
+#     return inner
+
+
+# @header # say = header(say)
+# @table
+# def say(name, surname, age):
+#     print("hello", name, surname, age)
+
+# say('Vasya', 'Ivanov', 30)
+
+
+def testTime(fn):
+    def wrapper(*args):
+        st = time.time()
+        fn(*args)
+        dt = time.time() - st
+        print(f"Время работы: {dt} сек")
+
