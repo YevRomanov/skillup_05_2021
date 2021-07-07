34f10009549db2d15ee5ae8492757c49f4dd2891
YevRomanov <yevromanov@gmail.com>
2021-07-07 12:31:37 +0200

    commit

--- Lesson_3.4/Task_1.py
+++ Lesson_3.4/Task_1.py
@@ -1,4 +1,6 @@
-
+#######################################Task 1#######################################
+#
+# 
 def driver_connection(ip, port):
     def decorator(func):
         def inner(doc):
@@ -8,3 +10,15 @@ def driver_connection(ip, port):
         return inner
     return decorator
 
+@driver_connection(ip="11.10.11.10", port="6699")
+def hp(doc):
+    print("I am HP printer")
+    print(f'Printing: {doc}')
+    
+@driver_connection(ip="11.11.10.11", port=7744)
+def canon(doc):
+    print("I am Canon printer")
+    print(f'Printing: {doc}')
+
+hp('Test text')
+canon('Test text_2')
