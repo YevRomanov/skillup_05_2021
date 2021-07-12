d9f8ef5e257049d1f2ac669dc4bd4768591e0cc7
YevRomanov <yevromanov@gmail.com>
2021-07-12 09:34:40 +0200

    commit

---
+++ Lesson_3.6/text1.py
@@ -0,0 +1,4 @@
+'''Today, Silicon Valley is still the home of the computer industry; 
+it is still full of high technology, but it is not the only center for high-tech in the USA.  
+Today here are computer firms all over the USA.... and all over the world; 
+but Silicon Valley still has the largest concentration of high-tech companies and research centers.'''
