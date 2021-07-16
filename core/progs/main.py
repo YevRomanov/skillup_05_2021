7d80e5231778f7b51449e719ee45abf019ffde4c
YevRomanov <yevromanov@gmail.com>
2021-07-16 12:04:04 +0200

    commit

--- core/progs/main.py
+++ core/progs/main.py
@@ -1,7 +1,18 @@
 
 
-input_list = [1,2,3,4,5,6,7,8,9,10]
+# input_list = [1,2,3,4,5,6,7,8,9,10]
 
-prime_numbers = get_prime_numbers_count(input list)
+# prime_numbers = get_prime_numbers_count(input list)
 
-print(prime_numbers)
+# print(prime_numbers)
+
+# open("readme.txt", mode = "w")
+# file = open("readme.txt", mode = "w")
+# file.write('1')
+# file.close()
+
+
+file = open("readme.txt")
+data = file.read()
+print(data)
+file.close()
